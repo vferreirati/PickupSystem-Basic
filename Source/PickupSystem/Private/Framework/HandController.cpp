c8a36b56ba315081a209a4bc64d06202079cf3ed
Victor Ferreira <vferreirati@gmail.com>
2018-07-31 11:44:45 -0300

    HandController actor class

---
+++ Source/PickupSystem/Private/Framework/HandController.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HandController.h"
+#include "Components/SceneComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/SphereComponent.h"
+#include "MotionControllerComponent.h"
+
+
+// Sets default values
+AHandController::AHandController()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComp = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	SetRootComponent(RootComp);
+
+	MotionControllerComp = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("MotionControllerComp"));
+	MotionControllerComp->SetupAttachment(RootComp);
+
+	HandMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("HandMesh"));
+	HandMesh->SetupAttachment(MotionControllerComp);
+
+	GrabSphere = CreateDefaultSubobject<USphereComponent>(TEXT("GrabSphere"));
+	GrabSphere->SetupAttachment(HandMesh);
+}
+
+// Called when the game starts or when spawned
+void AHandController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AHandController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AHandController::SetHand(EControllerHand Hand) {
+	MotionControllerComp->SetTrackingSource(Hand);
+}
---
+++ Source/PickupSystem/Public/Framework/HandController.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "HandController.generated.h"
+
+UCLASS()
+class PICKUPSYSTEM_API AHandController : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AHandController();
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void SetHand(EControllerHand Hand);
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class USceneComponent* RootComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class UMotionControllerComponent* MotionControllerComp;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class USkeletalMeshComponent* HandMesh;
+
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class USphereComponent* GrabSphere;
+
+protected:	
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	
+	
+};
